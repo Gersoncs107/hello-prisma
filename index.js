a16f406f268378043f9190c3069f06a6b1051d78
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-12 22:14:20 -0300

    schema.prisma

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
-  // ... you will write your Prisma Client queries here
-}
+    // ... you will write your Prisma Client queries here
+    const allUsers = await prisma.user.findMany()
+    console.log(allUsers)
+  }
 
 main()
   .then(async () => {
