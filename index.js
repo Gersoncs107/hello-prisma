a124e18a846a8548fc8fcead941e7369eca5c091
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-13 21:54:34 -0300

    schema.prisma

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const prisma = new PrismaClient()
 
 async function main() {
     // ... you will write your Prisma Client queries here
-    await prisma.user.create({
-      data: {
-        name: 'Alice',
-        email: 'alice@prisma.io',
-        posts: {
-          create: {title: 'Hello World'}
-        },
-        profile: {
-          create: {bio: 'I like turtles'}
-        }
-      }     
-    })
+    // await prisma.user.create({
+    //   data: {
+    //     name: 'Alice',
+    //     email: 'alice@prisma.io',
+    //     posts: {
+    //       create: {title: 'Hello World'}
+    //     },
+    //     profile: {
+    //       create: {bio: 'I like turtles'}
+    //     }
+    //   }     
+    // })
 
-    const allUsers = await prisma.user.findMany({
-      include: {
-        posts: true,
-        profile: true
-      }
-    })
-    console.dir(allUsers, { depth: null })
+    // const allUsers = await prisma.user.findMany({
+    //   include: {
+    //     posts: true,
+    //     profile: true
+    //   }
+    // })
+    // console.dir(allUsers, { depth: null })
 
     const post = await prisma.post.update({
       where: { id: 1 },
